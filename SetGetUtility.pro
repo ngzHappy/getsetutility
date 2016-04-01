358e8054290a4e19a039fca3b1d7c0de8ffbfaec
ngzHappy <819869472@qq.com>
2016-04-01 09:23:38 +0800

    +v 0.0.0

---
+++ SetGetUtility.pro
@@ -0,0 +1,15 @@
+CONFIG += c++14
+
+QT += core gui
+QT += widgets
+
+TARGET = setgetutility
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp \
+    SetGetUtility.cpp
+
+HEADERS  += MainWindow.hpp \
+    SetGetUtility.hpp
---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include "MainWindow.hpp"
+#include <QApplication>
+#include <QtGui/qimage.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    {
+        QImage * image = new QImage;
+        delete image;
+    }
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
+}
